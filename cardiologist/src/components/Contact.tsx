81916c6c8abae83f10110202f8fd997454e19112
Nikita Ingle <nikitaingle1311@gmail.com>
2025-07-15 17:37:20 +0530

    Removed _next redirect from contact form

--- cardiologist/src/components/Contact.tsx
+++ cardiologist/src/components/Contact.tsx
@@ -130,7 +130,7 @@ const Contact = () => {
                 className="space-y-6"
               >
                 <input type="hidden" name="_captcha" value="false" />
-                <input type="hidden" name="_next" value="https://akshitdhake.info/thankyou" />
+                <input type="hidden" name="_next" value="https://https://dr-manish-portfolio.vercel.app/thankyou" />
                 <input type="hidden" name="_subject" value="New Message from Ganwani Heart Care Website" />
                 {/* <input
     type="hidden"
